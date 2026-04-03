d4d93f2b6decf97123acb2f778690cdfd0349ff5
Wilson <wilsonyu657@gmail.com>
2026-04-02 21:31:05 -0400

    Create temporary response for search API

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,3 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {}
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    res.status(200).json({ message: "This will be the endpoint for search requests!" });
+}
