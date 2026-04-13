257e09a69d78c4fc828dc496da6f75fd74d41867
Wilson <wilsonyu657@gmail.com>
2026-04-12 20:57:44 -0400

    Add zooming to grid

--- app/components/nodes/surface.tsx
+++ app/components/nodes/surface.tsx
@@ -12,44 +12,68 @@ gsap.registerPlugin(Draggable, InertiaPlugin);
 const ZOOM_INTENSITY = 0.0015;
 const ZOOM_MAX = 2;
 const ZOOM_MIN = 0.5;
+const ZOOM_DURATION = 0.5;
 
-type Bounds = { w: number; h: number; max: number };
-const BOUNDS_ZERO: Bounds = { w: 0, h: 0, max: 0 };
+type Bounds = { x: number; y: number; max?: number };
+const BOUNDS_ZERO: Bounds = { x: 0, y: 0, max: 0 };
+
+type Mounted = {
+    drag: boolean;
+    scale: boolean;
+};
 
 export default function Surface() {
     const container = useRef<HTMLDivElement>(null);
     const surface = useRef<HTMLDivElement>(null);
     const scaledSurface = useRef<HTMLDivElement>(null);
     const [size, setSize] = useState<Bounds>(BOUNDS_ZERO);
-    const lastSize = useRef(BOUNDS_ZERO);
+    const [zoom, setZoom] = useState(ZOOM_MIN);
+    const zoomPosition = useRef<Bounds>(BOUNDS_ZERO);
 
     useLayoutEffect(() => {
         const updateMinWidth = () => {
-            const w = window.innerWidth;
-            const h = window.innerHeight;
-            setSize({ w, h, max: Math.max(w, h) });
+            const x = window.innerWidth;
+            const y = window.innerHeight;
+            setSize({ x, y, max: Math.max(x, y) });
         };
         updateMinWidth();
         window.addEventListener("resize", updateMinWidth);
 
-        return () => window.removeEventListener("resize", updateMinWidth);
+        const updateScale = (deltaScale: number) => {
+            setZoom((prev) => Math.min(ZOOM_MAX, Math.max(ZOOM_MIN, prev + deltaScale)));
+        };
+
+        const onWheel = (e: WheelEvent) => {
+            e.preventDefault();
+            zoomPosition.current.x = e.clientX;
+            zoomPosition.current.y = e.clientY;
+            updateScale(-e.deltaY * ZOOM_INTENSITY);
+        };
+
+        window.addEventListener("wheel", onWheel, { passive: false });
+
+        return () => {
+            window.removeEventListener("resize", updateMinWidth);
+            window.removeEventListener("wheel", onWheel);
+        };
     }, []);
 
     useGSAP(() => {
         gsap.fromTo(surface.current, { opacity: 0 }, { opacity: 1, duration: 0.5 });
     }, []);
 
-    const mounted = useRef(false);
+    const mounted = useRef<Mounted>({ drag: false, scale: false });
     useGSAP(
         () => {
-            if (!container.current || !surface.current || size.max <= 0) return;
+            if (!container.current || !surface.current || (size.max ?? 0) <= 0) return;
 
-            if (mounted.current) {
+            if (mounted.current.drag) {
             } else {
                 const bounds = container.current.getBoundingClientRect();
-                const centerX = (bounds.width - size.w) / 2;
-                const centerY = (bounds.height - size.h) / 2;
-                gsap.set(surface.current, { x: centerX, y: centerY });
+                const centerX = (bounds.width - size.x / ZOOM_MIN) / 2;
+                const centerY = (bounds.height - size.y / ZOOM_MIN) / 2;
+                // gsap.set(surface.current, { x: centerX, y: centerY });
+                gsap.set(scaledSurface.current, { x: centerX, y: centerY });
 
                 const handleFocus = () => {
                     if (!document.activeElement || document.activeElement.className.includes("surface")) return;
@@ -61,22 +85,57 @@ export default function Surface() {
                     inertia: true,
                     onPress: handleFocus,
                 });
-                mounted.current = true;
+                mounted.current.drag = true;
             }
-            lastSize.current = size;
         },
         { scope: container, dependencies: [size] },
     );
 
+    useGSAP(() => {
+        if (!container.current || !scaledSurface.current || !surface.current || (size.max ?? 0) <= 0) return;
+        if (!mounted.current.scale && container.current) {
+            gsap.set(scaledSurface.current, { scale: zoom });
+            mounted.current.scale = true;
+        } else {
+            const bounds = container.current.getBoundingClientRect();
+            const lastZoom = gsap.getProperty(scaledSurface.current, "scale") as number;
+
+            // All x/y coordinates are anchored to the element's center, and relative to the screen's center
+
+            const currentX = gsap.getProperty(surface.current, "x") as number;
+            const currentY = gsap.getProperty(surface.current, "y") as number;
+
+            const desiredX = (zoomPosition.current.x - size.x / 2) / lastZoom;
+            const desiredY = (zoomPosition.current.y - size.y / 2) / lastZoom;
+
+            const scaleRatio = zoom / lastZoom;
+
+            const newX = currentX - desiredX * (scaleRatio - 1);
+            const newY = currentY - desiredY * (scaleRatio - 1);
+
+            const maxWidth = (size.x / ZOOM_MIN) * zoom;
+            const maxHeight = (size.y / ZOOM_MIN) * zoom;
+
+            const maxX = (maxWidth - bounds.width) / 2 / zoom;
+            const maxY = (maxHeight - bounds.height) / 2 / zoom;
+
+            const clampedX = gsap.utils.clamp(-maxX, maxX, newX);
+            const clampedY = gsap.utils.clamp(-maxY, maxY, newY);
+
+            gsap.to(scaledSurface.current, { scale: zoom, duration: ZOOM_DURATION, ease: "power2.out" });
+            gsap.to(surface.current, { x: clampedX, y: clampedY, duration: ZOOM_DURATION, ease: "power2.out" });
+        }
+    }, [zoom, size]);
+
     return (
-        <div ref={container} className="relative z-1 w-full h-full shrink-0 overflow-hidden">
-            <div ref={scaledSurface} className="absolute origin-center flex justify-center items-center" style={{ width: size.w, height: size.h }}>
+        <div ref={container} className="relative z-1 w-full h-full shrink-0 overflow-clip antialiased">
+            <div ref={scaledSurface} className="absolute flex justify-center items-center" style={{ width: size.x / ZOOM_MIN, height: size.y / ZOOM_MIN }}>
                 <div
                     ref={surface}
-                    className="surface w-full h-full origin-center"
+                    className="absolute surface w-full h-full origin-center"
                     style={{
-                        backgroundSize: `${size.max * 0.05}px ${size.max * 0.05}px`,
-                        backgroundImage: `radial-gradient(#212830 ${size.max * 0.002}px,transparent 1px)`,
+                        backgroundSize: `${(size.max ?? 0) * 0.05}px ${(size.max ?? 0) * 0.05}px`,
+                        backgroundImage: `radial-gradient(#212830 ${(size.max ?? 0) * 0.003}px,transparent 1px)`,
                     }}
                 ></div>
             </div>
