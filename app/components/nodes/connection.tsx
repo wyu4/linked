b33e144a7886c6c56b998868acb506cefe5b6018
Wilson <wilsonyu657@gmail.com>
2026-04-16 08:39:51 -0400

    Add colored borders

--- app/components/nodes/connection.tsx
+++ app/components/nodes/connection.tsx
@@ -103,10 +103,15 @@ const Connection = forwardRef<
             <div
                 ref={(node) => bindRefAndForwardRef(node, forwardedRef, ref)}
                 className={
-                    "node absolute aspect-square bg-font border-4 border-font rounded-full overflow-y-visible overflow-x-hidden flex items-center justify-center z-20" +
+                    "node absolute aspect-square bg-font border-6 border-font rounded-full overflow-y-visible overflow-x-hidden flex items-center justify-center z-20" +
                     className
                 }
-                style={{ x: initialPos[0], y: initialPos[1], width: calculateNodeWidth(size.x) }}
+                style={{
+                    x: initialPos[0],
+                    y: initialPos[1],
+                    width: calculateNodeWidth(size.x),
+                    borderColor: data.type === "from" ? "#FF0000" : data.type === "to" ? "#00FF00" : undefined,
+                }}
                 {...props}
             >
                 <img src={`https://github.com/${data.login}.png`} />
